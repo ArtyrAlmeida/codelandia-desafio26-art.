763cde20255f4dd7482f4541b91376f9cb342f81
Artur Almeida <arturoalmeidafreire@gmail.com>
2022-07-16 19:32:06 -0300

    view: adiciona componente de footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            <h2>Art.</h2>
+            <nav>
+                <a>INICIO</a>
+                <a>BUSCAR</a>
+                <a>EXPLORAR</a>
+                <a>SOBRE NÓS</a>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
